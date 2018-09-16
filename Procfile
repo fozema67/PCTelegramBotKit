4e914f73d3d3f3a2728267b8adbc6c4f4b65d4e9
fozema67 <fozema67@gmail.com>
2018-09-16 22:17:00 +0300

    Бета версия 0.3.1 Сборка 178
    - Бот переехал на новый адрес.
    - Добавлены команды для 185 группы.
    - Добавлена поддержка еще нескольких групп 1-го курса.
    - Удалены лишние команды.
    - Удалены лишние модули.
    - Устранены баги и внедрены различные улучшения связанные с отзывчивостью бота.
    - Добавлен модуль администратора и некоторые важные команды.
    - Скорость парсинга сайта spbkit.edu.ru возросла с 13 сек до 8 сек.
    - Теперь бот работает на своем сервере, а не локальном.
    - Код бота стал читабельнее. Скоро исходники появятся в открытом доступе на Github.
    
    Signed-off-by: fozema67 <fozema67@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: (python -m http.server $PORT &) && (python3 bot.py)
--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-tokenTel = '599363626:AAHqMKVzwDj-CPO7OM8aJ3X0RdKFdxHP9J8'
-tokenDial = '8f4db732a10244b2b82804ef1560f728'
-APIKeyOWM = '6de3ddbc7b036e4a167d5c3156ac1ac6'
+tokenTel = '508307190:AAHk6zzXnaCeyvKvxFOfHElCNpX7fSGcLYw'
 
-referenseBot = '@test_fozema67Bot'
+referenseBot = '@datatable_fozema67Bot'
 replacement = '5.19.239.65:8080/replacements/view.html'
 schedule = [ 'None', '09:00 - 09:45, 09:50 - 10:35', '10:55 - 11:40, 11:45 - 12:30', '13:00 - 13:45, 13:50 - 14:35', '14:45 - 15:30, 15:35 - 16:20']
 
