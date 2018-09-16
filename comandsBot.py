b2df5805714a86905ed5fa1b363187f0a942a68a
fozema67 <fozema67@gmail.com>
2018-09-16 22:59:34 +0300

    Бета версия 0.3.1 Сборка 182
    - Бот переехал на новый адрес.
    - Добавлены команды для 185 группы.
    - Добавлена поддержка еще нескольких групп 1-го курса.
    - Удалены лишние команды.
    - Удалены лишние модули.
    - Устранены баги и внедрены различные улучшения связанные с отзывчивостью бота.
    - Добавлен модуль администратора и некоторые важные команды.
    - Скорость парсинга сайта spbkit.edu.ru возросла с 13 сек до 8 сек.
    - Теперь бот работает на своем сервере, а не локальном.
    - Код бота стал читабельнее. Скоро исходники появятся в открытом доступе на Github.
    
    Signed-off-by: fozema67 <fozema67@gmail.com>

--- comandsBot.py
+++ comandsBot.py
@@ -25,7 +25,7 @@ def messages(message):
     elif message.text == "/key_t" or message.text == "/key_t" + config.referenseBot:
         cmd_admin(message)
     elif message.text == "/about" or message.text == "/about" + config.referenseBot:
-        bot.send_message(message.chat.id, "version: " + config.version + "build: " + config.build + "\nbuild date: " + config.builddate)
+        bot.send_message(message.chat.id, "version: " + config.version + " build: " + config.build + "\nbuild date: " + config.builddate)
     elif message.text == "/help" or message.text == "/help" + config.referenseBot:
         bot.send_message(message.chat.id, "/replacement - замены в расписании (актуально для всех групп)" +
                                           "\n/schedule - расписание пар" +
@@ -34,29 +34,30 @@ def messages(message):
                                           "\n/185wednesday - расписание на среду 185 группа" +
                                           "\n/185thursday - расписание на четверг 185 группа" +
                                           "\n/185friday - расписание на пятницу 185 группа" +
-                                          "\n/185saturday - расписание на субботу 185 группа")
+                                          "\n/185saturday - расписание на субботу 185 группа" +
+                                          "\n/about - О боте")
     elif message.text == "/replacement":
         bot.send_message(message.chat.id, "Замены в расписании находятся на этом сайте:")
         bot.send_message(message.chat.id, config.replacement)
-    elif message.text == "/schedule":
+    elif message.text == "/schedule" or message.text == "/schedule" + config.referenseBot:
         for i in range(0, 5):
             bot.send_message(message.chat.id,  "Pair " + str(i+1) + ": " + str(config.schedule[i+1]))
-    elif message.text == '/185monday':
+    elif message.text == '/185monday' or message.text == "/185monday" + config.referenseBot:
         for i in range(1, 9):
             bot.send_message(message.chat.id, "Lesson " + str(i) + ": " + str(parsing.pars.monday185[i]))
-    elif message.text == '/185tuesday':
+    elif message.text == '/185tuesday' or message.text == "/185tuesday" + config.referenseBot:
         for i in range(1, 9):
             bot.send_message(message.chat.id, "Lesson " + str(i) + ": " + str(parsing.pars.tuesday185[i]))
-    elif message.text == '/185wednesday':
+    elif message.text == '/185wednesday' or message.text == "/185wednesday" + config.referenseBot:
         for i in range(1, 9):
             bot.send_message(message.chat.id, "Lesson " + str(i) + ": " + str(parsing.pars.wednesday185[i]))
-    elif message.text == '/185thursday':
+    elif message.text == '/185thursday' or message.text == "/185thursday" + config.referenseBot:
         for i in range(1, 9):
             bot.send_message(message.chat.id, "Lesson " + str(i) + ": " + str(parsing.pars.thursday185[i]))
-    elif message.text == '/185friday':
+    elif message.text == '/185friday' or message.text == "/185friday" + config.referenseBot:
         for i in range(1, 9):
             bot.send_message(message.chat.id, "Lesson " + str(i) + ": " + str(parsing.pars.friday185[i]))
-    elif message.text == '/185saturday':
+    elif message.text == '/185saturday' or message.text == "/185saturday" + config.referenseBot:
         for i in range(1, 9):
             bot.send_message(message.chat.id, "Lesson " + str(i) + ": " + str(parsing.pars.saturday185[i]))
     else:
