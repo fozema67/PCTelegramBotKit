28903e6c426b6477f5ac2009131e172683eb8142
fozema67 <fozema67@gmail.com>
2018-09-27 22:20:22 +0300

    Бета версия 0.3.2 Сборка 196
    - Бот переехал на новый адрес.
    - Добавлены команды для 185 группы.
    - Добавлена поддержка еще нескольких групп 1-го курса.
    - Удалены лишние команды.
    - Удалены лишние модули.
    - Устранены баги и внедрены различные улучшения связанные с отзывчивостью бота.
    - Добавлен модуль администратора и некоторые важные команды.
    - Скорость парсинга сайта spbkit.edu.ru возросла с 13 сек до 8 сек.
    - Теперь бот работает на своем сервере, а не локальном.
    - Исправлен баг парсинга сайта.
    - Код бота стал читабельнее. Скоро исходники появятся в открытом доступе на Github.
    
    Signed-off-by: fozema67 <fozema67@gmail.com>

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ tokenTel = '508307190:AAHk6zzXnaCeyvKvxFOfHElCNpX7fSGcLYw'
 
 referenseBot = '@datatable_fozema67Bot'
 version = '0.3.2'
-build = '191'
-builddate = '27.09.2018 18:17'
+build = '196'
+builddate = '27.09.2018 22:19'
 replacement = '46.17.43.100:8080/replacements/view.html'
 schedule = [ 'None', '09:00 - 09:45, 09:50 - 10:35', '10:55 - 11:40, 11:45 - 12:30', '13:00 - 13:45, 13:50 - 14:35', '14:45 - 15:30, 15:35 - 16:20']
 
